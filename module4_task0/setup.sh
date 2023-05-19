9c8adaea8584976d161b2cf2c00aa36288ff3e2f
jgnacio <jgnaciogomez@gmail.com>
2023-05-19 16:31:14 -0300

    Install node with nvm

--- module4_task0/setup.sh
+++ module4_task0/setup.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/bash
 sudo apt-get update && sudo apt-get upgrade -y;
-sudo apt-get install -y wget python3 python3-pip git zip;
+sudo apt-get install -y python3 python3-pip git zip;
 
 # Install hugo
 sudo mkdir /app/hugo_bin;
@@ -14,16 +14,16 @@ curl -sSfL https://raw.githubusercontent.com/golangci/golangci-lint/master/insta
 sudo mv ./bin/golangci-lint /usr/bin/;
 sudo rm -rf ./bin
 
-# nodejs install with nvm
-wget -qO- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.3/install.sh | bash;
-export NVM_DIR="$([ -z "${XDG_CONFIG_HOME-}" ] && printf %s "${HOME}/.nvm" || printf %s "${XDG_CONFIG_HOME}/nvm")"
-[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh" # This loads nvm
-nvm install 18.16.0
-nvm use 18
-nvm version
+# Nodejs install with nvm
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.3/install.sh | bash
+export NVM_DIR="$([ -z "${XDG_CONFIG_HOME-}" ] && printf %s "${HOME}/.nvm" || printf %s "${XDG_CONFIG_HOME}/nvm")";
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh";
+nvm install "18.16.0";
+nvm use 18;
+nvm version;
 
 # Install markdown-link-cli and markdown-link-check
-sudo npm install -g markdownlint-cli markdown-link-check;
+npm install -g markdownlint-cli markdown-link-check;
 
 # W3C validator dependencies
-sudo python3 -m pip install requests
+python3 -m pip install requests
