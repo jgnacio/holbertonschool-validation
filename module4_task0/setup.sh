fb4e855515dd114c2686490552e35bf88c07ac28
jgnacio <jgnaciogomez@gmail.com>
2023-05-19 16:10:58 -0300

    Install: Nodejs with NVM

--- module4_task0/setup.sh
+++ module4_task0/setup.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/bash
 sudo apt-get update && sudo apt-get upgrade -y;
-sudo apt-get install -y wget python3 python3-pip git zip nodejs;
+sudo apt-get install -y wget python3 python3-pip git zip;
 
 # Install hugo
 sudo mkdir /app/hugo_bin;
@@ -13,6 +13,14 @@ sudo rm -rf /app/hugo_bin;
 curl -sSfL https://raw.githubusercontent.com/golangci/golangci-lint/master/install.sh | sh -s;
 sudo mv ./bin/golangci-lint /usr/bin/;
 sudo rm -rf ./bin
+
+# nodejs install with nvm
+wget -qO- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.3/install.sh | bash;
+export NVM_DIR="$([ -z "${XDG_CONFIG_HOME-}" ] && printf %s "${HOME}/.nvm" || printf %s "${XDG_CONFIG_HOME}/nvm")"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh" # This loads nvm
+nvm install 18.16.0
+nvm use 18
+nvm version
 
 # Install markdown-link-cli and markdown-link-check
 sudo npm install -g markdownlint-cli markdown-link-check;
