5e3c7d66a7a21c6c0d92f00519c54ce7d1f64cd1
jgnacio <jgnaciogomez@gmail.com>
2023-05-20 18:03:25 -0300

    Be faster Upgrade: install only the necessary dependencies (faster)

--- module4_task1/setup.sh
+++ module4_task1/setup.sh
@@ -1,18 +1,6 @@
 #!/usr/bin/bash
-sudo apt-get update && sudo apt-get upgrade -y;
-sudo apt-get install -y wget python3 python3-pip git zip;
-
-# Install hugo
-sudo mkdir /app/hugo_bin;
-sudo wget https://github.com/gohugoio/hugo/releases/download/v0.84.0/hugo_extended_0.84.0_Linux-64bit.tar.gz -P /app/hugo_bin/;
-sudo tar -xf /app/hugo_bin/hugo_extended_0.84.0_Linux-64bit.tar.gz -C /app/hugo_bin/;
-sudo mv /app/hugo_bin/hugo /bin/;
-sudo rm -rf /app/hugo_bin;
-
-# Install golangcli-lint
-curl -sSfL https://raw.githubusercontent.com/golangci/golangci-lint/master/install.sh | sh -s;
-sudo mv ./bin/golangci-lint /usr/bin/;
-sudo rm -rf ./bin
+sudo apt-get update
+sudo apt-get install -y python3 python3-pip git zip;
 
 # Install markdown-link-cli and markdown-link-check
 npm install -g markdownlint-cli markdown-link-check;
